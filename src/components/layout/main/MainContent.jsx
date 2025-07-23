5b67db7a9722882e52c7cec893565489e183f7fe
Arina Smirnova <arrrkkka@yandex.ru>
2025-07-24 02:26:54 +0300

    ипшнм

--- src/components/layout/main/MainContent.jsx
+++ src/components/layout/main/MainContent.jsx
@@ -2,14 +2,35 @@ import { Layout } from 'antd';
 import BlockForModalSmall from '../../common/block-for-main/BlockForMainSmall';
 import BlockForModalMidle from '../../common/block-for-main/BlockForMainMidle';
 import BlockForMainLarge from '../../common/block-for-main/BlockForMainLarge';
+import ModalWindow from '../../common/cases-modal/ModalWindow';
 
 export default function CasesContent({ tg, profile, cases }) {
+  const [isModalOpen, setIsModalOpen] = useState({
+    isOpen: false,
+    currentItem: null,
+  });
+
+  const showModal = (item) => {
+    setIsModalOpen({
+      isOpen: true,
+      currentItem: item,
+    });
+  };
+
+  const handleOk = () => {
+    setIsModalOpen({ ...isModalOpen, isOpen: false });
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen({ ...isModalOpen, isOpen: false });
+  };
   return (
     <Layout.Content className="body-content-style">
       <Layout className="case-map-style">
         <BlockForModalSmall
           title="Give a ways"
           className="blue-card-style-modal"
+          onClick={() => showModal(cases)}
         />
         <BlockForModalSmall
           title="Follow the promotions!"
@@ -31,6 +52,13 @@ export default function CasesContent({ tg, profile, cases }) {
           className="blue-card-style-modal"
         />
       </Layout>
+      <ModalWindow
+        cardData={isModalOpen.currentItem}
+        isOpen={isModalOpen.isOpen}
+        onOk={handleOk}
+        onClose={handleCancel}
+        item={''}
+      />
     </Layout.Content>
   );
 }
